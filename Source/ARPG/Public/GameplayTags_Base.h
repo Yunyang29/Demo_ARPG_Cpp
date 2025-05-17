b19a022ce1f9387a6fbf04411e7063b901a259b4
fyy <esheep292929@gmail.com>
2025-05-17 16:39:28 +0800

    light and heavy attack initial

--- Source/ARPG/Public/GameplayTags_Base.h
+++ Source/ARPG/Public/GameplayTags_Base.h
@@ -10,10 +10,15 @@ namespace GameplayTags_Base
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look); // 视角输入标签
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_EquipAxe); // 装备
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_UnequipAxe); // 装备
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_LightAttack_Axe); // 装备
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_HeavyAttack_Axe); // 装备
 
 	/**Player Tags**/
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Ability_Equip_Axe);
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Ability_Unequip_Axe);
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Ability_Attack_Light_Axe);
+	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Ability_Attack_Heavy_Axe);
+	
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Weapon_Axe);
 
 	ARPG_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Equip_Axe);
