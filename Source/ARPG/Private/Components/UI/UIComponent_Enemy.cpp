217658eb0441306b0fc879aa3800465357f0221a
Yunyang <155894442+Yunyang29@users.noreply.github.com>
2025-10-13 10:17:08 +0100

    boss set up

--- Source/ARPG/Private/Components/UI/UIComponent_Enemy.cpp
+++ Source/ARPG/Private/Components/UI/UIComponent_Enemy.cpp
@@ -1,5 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Components/UI/UIComponent_Enemy.h"
 
+#include "Widgets/Widget_Base.h"
+
+void UUIComponent_Enemy::RegisterWidget(UWidget_Base* InWidget)
+{
+	Widgets.Add(InWidget);
+}
+
+void UUIComponent_Enemy::RemoveWidgetsIfAny()
+{
+	if (Widgets.IsEmpty())
+	{
+		return;
+	}
+
+	for (UWidget_Base* Widget : Widgets)
+	{
+		if (Widget)
+		{
+			Widget->RemoveFromParent();
+		}
+	}
+}
--- Source/ARPG/Public/Components/UI/UIComponent_Enemy.h
+++ Source/ARPG/Public/Components/UI/UIComponent_Enemy.h
@@ -1,11 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/UI/UIComponent_Base.h"
 #include "UIComponent_Enemy.generated.h"
 
+class UWidget_Base;
 /**
  * 
  */
@@ -13,5 +12,14 @@ UCLASS()
 class ARPG_API UUIComponent_Enemy : public UUIComponent_Base
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterWidget(UWidget_Base* InWidget);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveWidgetsIfAny();
+
+private:
+	TArray<UWidget_Base*> Widgets;
 };
